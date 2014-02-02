0ef9b8fbfad4b507c35cbbaf37c189eee19fdbce
Matt Senn <eternalmatt@gmail.com>
2014-02-01 20:12:59 -0500

    Added validation to modal popup

--- public/js/controllers/PopupController.js
+++ public/js/controllers/PopupController.js
@@ -1,19 +1,50 @@
 angular.module('passport.controller.popup', [])
-    .controller('PopupController', function($scope, $modalInstance, factory){
-      $scope.factory = factory;
-      $scope.button = { model: 'generate' };
-      
-      $scope.ok = function(action, factory){
-        if (action === 'generate'){
-          factory.$update(); 
-        } else if (action === 'delete'){
-          factory.$remove();
+  .controller('PopupController', function($scope, $modalInstance, factory) {
+    var factoryResource = factory;
+    $scope.factory = { name : factory.name, number : factory.nodes.length };
+    $scope.button = { model : 'generate' };
+    $scope.form = {};
+  
+    $scope.numRequiredError = function(form) {
+      return form.number.$dirty && form.number.$error.required;
+    };
+  
+    $scope.numOutOfRangeError = function(form) {
+      if ($scope.numRequiredError(form)) { return false; }
+      return form.number.$dirty && form.number.$error.validRange;
+    };
+  
+    $scope.nameRequiredError = function(form) {
+      return form.name.$dirty && form.name.$error.required;
+    };
+  
+    $scope.ok = function(action, factory) {
+      angular.extend(factoryResource, factory);
+      if (action === 'generate') {
+        factoryResource.$update();
+      } else if (action === 'delete') {
+        factoryResource.$remove();
+      }
+      $modalInstance.close();
+    };
+  
+    $scope.cancel = function() {
+      $modalInstance.dismiss();
+    };
+  
+  })
+  
+  .directive('validRange', function() {
+    return {
+      require : 'ngModel',
+      link : function(scope, element, attrs, controller) {
+        function validate(value) {
+          var valid = value >= 1 && value <= 15;
+          controller.$setValidity('validRange', valid);
+          return valid ? value : undefined;
         }
-        $modalInstance.close();
-      };
-      
-      $scope.cancel = function(){
-        $modalInstance.dismiss();
-      };
-
-    })
+  
+        controller.$parsers.unshift(validate);
+      }
+    };
+  })
